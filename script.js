856a30434fdd3e74ecab01258dae209b4dff2455
DevineSploit <166527064+DevineSploit@users.noreply.github.com>
2026-08-20 21:12:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const id=a.getAttribute('href');if(!id||id==='#')return;const t=document.querySelector(id);if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth'});}}));
